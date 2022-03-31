72e9ccf504dd5efa3bb1e02122a55a0fbfe9d61b
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-03-31 14:36:25 +0530

    arithmatic operation

---
+++ C Program/multiplication program.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,res;
+	printf("enter any two number : \n");
+	scanf("%d %d",&num1,&num2);
+	res=nu1m*num2;
+	printf("multiplication is = %d",res);
+}
---
+++ C Program/substraction program.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,res;
+	printf("enter any two number : \n");
+	scanf("%d %d",&num1,&num2);
+	res=num1-num2;
+	printf("Subtraction is = %d",res);
+}
