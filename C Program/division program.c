6ee2654a79d9510dfcc48de5fa948348f605e6ab
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-03-31 20:10:32 +0530

    arethmatic operation

--- C Program/division program.c
+++ C Program/division program.c
@@ -2,9 +2,9 @@
 #include<conio.h>
 void main()
 {
-	int num1,num2,res;
-	printf("enter any two number : \n");
-	scanf("%d %d",&num1,&num2);
-	res=num1/num2;
-	printf("division is = %d",res);
+	int mass,accl,force;
+	printf("enter the mass and accl : \n");
+	scanf("%d %d",&mass,&accl);
+	force=mass*accl;
+	printf("force is = %d",force);
 }
