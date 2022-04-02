5ca9fc1480b8fda2cb3f832aebcff7866056eb3e
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-02 12:42:55 +0530

    arethmatic operation

---
+++ C Program/hemisphere.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int  r;
+	float pi=3.142,hs  ;
+	printf("enter  the value of radius  : " );
+	scanf("%d",&r);
+	hs= 3*pi*r*r;
+	printf("sphere area is = %f",hs);
+}
