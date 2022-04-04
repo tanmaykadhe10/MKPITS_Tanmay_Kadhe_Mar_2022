e2856c2cbfd3968aba772df8eefaaf4ef51034b5
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-04 16:52:44 +0530

    reverce operation

---
+++ C Program/reverce operation.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num,n1,n2,n3,rev;
+	printf("enter the  three digit number : "  );
+	scanf("%d",&num);
+	n1=num%10;
+	num=num/10;
+	
+	n2=num%10;
+	num=num/10;
+	
+	n3=num%10;
+	
+	rev=n1*100+n2*10+n3*1;
+	
+	printf("reverce=%d ",rev);
+}
+
+
