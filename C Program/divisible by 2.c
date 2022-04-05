a0d903fc77407f156f1b6a8b5f1c6428e0261a06
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-05 20:39:11 +0530

    dividing two number

---
+++ C Program/divisible by 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num1,num2,div;
+      printf("Enter Two Numbers\n");
+
+      printf("Enter First Number  : ");
+      scanf("%d", &num1);
+      
+      printf("\nEnter Second Number : ");
+      scanf("%d",&num2);
+      div=num1/num2;
+      printf("division is = %d",div);
+}
