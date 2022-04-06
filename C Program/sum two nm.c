8916db14a5efde3d213bb9a3044935fc72a6e1a0
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-06 06:38:04 +0530

    sum of ywo number

---
+++ C Program/sum two nm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+   {
+	int x, y, sum;
+	
+    printf("enter the first number: "); 
+    scanf("%d", &x);
+    
+    printf("enter the second number: ");
+    scanf("%d", &y);
+    
+    sum = x + y;
+    
+    printf("Sum of two number is = %d\n", sum);
+    
+}
