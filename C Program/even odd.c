2835342dc17f8fa3d03434d2548fc1e5898b9f61
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-07 09:23:17 +0530

    even odd number

---
+++ C Program/even odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x;
+	printf("enter the number:");
+	scanf("%d",&x);
+	
+	if (x%2==0)
+	{
+		printf("it is even number ");
+	}
+	
+	else
+	
+	printf("it is odd number ");
+	
+}
---
+++ C Program/posetive negarive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x;
+	printf("enter the number:");
+	scanf("%d",&x);
+	
+	if (x>0)
+	{
+		printf("it is posetive number ");
+	}
+	
+	else
+	
+	printf("it is negative number ");
+	
+}
