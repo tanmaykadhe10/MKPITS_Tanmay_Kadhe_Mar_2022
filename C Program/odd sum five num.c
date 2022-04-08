f59031afec62cdea3fc985aa960283c30bfd3e33
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-08 09:46:52 +0530

    odd even

---
+++ C Program/odd sum five num.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int num1,num2,num3,num4,num5,ores;
+   printf("enter the frist number :");
+   scanf("%d",&num1);
+   printf("enter the second number :");
+   scanf("%d",&num2);
+   printf("enter the third number :");
+   scanf("%d",&num3);
+   printf("enter the fourth number :");
+   scanf("%d",&num4);
+   printf("enter the fifth number :");
+   scanf("%d",&num5);
+   
+   if (num1%2!=0)
+   {
+   	  ores=ores+num1;
+   }
+   else if (num2%2!=0)
+   {
+   	  ores=ores+num2;
+   }
+   else if (num3%2!=0)
+   {
+   	  ores=ores+num3;
+   }
+   else if (num4%2!=0)
+   {
+   	  ores=ores+num4;
+   }
+   else if (num5%2!=0)
+   {
+   	  ores=ores+num5;
+   }
+   else
+   {
+   	printf("other num is even");
+   }
+   printf("sum of odd number=\n",ores);
+}
