d4dcb64a23e1ccc82a3eb219d15c98e9f0174e4f
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-11 16:18:54 +0530

    amstrong num usin loop

---
+++ C Program/amstrong loop.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num,a,i,b,c,n1,n2,n3,sum;
+	
+	i=1;
+	
+	while(i<=999)
+	{
+		n1=i-((i/10)*10);
+		n2=((i/10)-(i/100)*10);
+		n3=((i/100)-(1/1000));
+		
+		sum=(n1*n1*n1)+(n2*n2*n2)+(n3*n3*n3);
+		 if(sum==i)
+		{
+			printf("\n number is amstrong :%d",sum);
+		}
+		i++;
+	}
+	
+}
+	
