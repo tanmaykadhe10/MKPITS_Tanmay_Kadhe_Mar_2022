b92ae9c8704e36f57e9fa072abc4a7f234c1e859
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-12 08:26:56 +0530

    asending decending q31

---
+++ C Program/q31.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("enter the two number :");
+	scanf("%d %d",&a,&b);
+	if (a!= b)
+	{
+		
+		if(a,b)
+		{
+			printf("it  is assending order \n");
+			
+		}
+		else
+		{
+			printf("it is descending order \n");
+		}
+	}
+}
