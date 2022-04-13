e21739d06b0547feeb898af0d9e4d8f3800ed500
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-13 07:39:11 +0530

    print c value using loop

---
+++ C Program/cls work print c.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,r;
+	a=0;
+	b=1;
+	
+	printf("enter r:");
+	scanf("%d",&r);
+	
+	printf("%d %d",a,b);
+	c=0;
+	while(c<=r)
+	{
+		c=a+b;
+		 
+		 if(c<=r)
+		 {
+		 	printf("%d \n",c);
+		 }
+		
+		a=b;
+		b=c;
+	}
+	
+	
+}
