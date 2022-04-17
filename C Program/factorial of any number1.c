08e4cb6d083e2a3e15a431b0af3008b96eea9835
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-17 15:56:37 +0530

    factorial of any number

---
+++ C Program/factorial of any number1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num,i,fact=1;
+	printf("Enter Number : ");
+	scanf("%d",&num);
+
+	i=1;
+	while(i<=1)
+	{
+		fact=fact*i;
+
+		i++;
+	}
+	printf("Factorial is %d ",fact);
+}
