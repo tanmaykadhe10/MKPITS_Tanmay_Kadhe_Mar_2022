57b6efa9a383c6ebedb86a80438c1177c2104cc4
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-17 16:18:39 +0530

    armstrong number find

---
+++ C Program/armstrong number find.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n1,num,r,res=0,n=0;
+	printf("Enter Any Number : ");
+    scanf("%d",&n1);
+	num=n1;
+    while(num!=0)
+    {
+        num=num/10;
+        n++;
+    }
+	num=n1;
+	while (num!=0)
+    {
+        r=num%10;
+        int c=1;
+        int i=0;
+        while(i<n)
+        {
+            c=c*r;
+            i=i+1;
+        }
+        res=res+c;
+        num=num/10;
+    }
+	if(res==n1)
+	{
+        printf("Number is Armstrong ! \n",n1);
+    }
+    else
+    {
+		printf("Number is not Armstrong ! \n", n1);
+    }
+} 
