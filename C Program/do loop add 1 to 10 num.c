a57d70ce6777f30edebd8672e55db29d81e29c0f
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-19 06:12:53 +0530

    do loop 1 to 10 num add

---
+++ C Program/do loop add 1 to 10 num.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i, add=0;
+	i=10;
+	
+	do
+	{
+			add= add+i;
+			
+			i-- ;
+			
+	}
+		while(i>=1);
+       {
+       		printf("%d",add);
+	   }
+	
+
+	
+}
