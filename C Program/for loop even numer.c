a9a12b4ba1ec3c3194be9fc0f9667a32bff7e344
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-19 20:17:54 +0530

    for loop even number

---
+++ C Program/for loop even numer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf("All Even Number between 1 and 50 : \n ");
+	for(a=1;a<=50;a++)
+	{	
+		if(a%2==0)
+		{
+		printf("%d ",a);
+		}
+	}
+}
