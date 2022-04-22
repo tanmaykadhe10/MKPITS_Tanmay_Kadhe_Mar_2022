dc9754041a9ea131fd632a8dfb4918fa7f4d91d3
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-22 14:40:00 +0530

    Que43 divisor value

---
+++ C Program/Que43.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,n;
+	
+	printf("Enter the integer :");
+	scanf("%d",&n);
+	
+	for(i=1; i<=n; i++)
+	{
+		if(n%i==0)
+		{
+			printf("%d\n",i);
+		}
+	}
+}
