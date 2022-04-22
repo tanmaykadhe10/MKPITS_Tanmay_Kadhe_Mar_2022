2d27392d632a2bf056a443ae96702e2211a849f0
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-22 08:55:09 +0530

    array reverse integer

---
+++ C Program/array reverse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[5] , n;
+	
+	printf("Enter the 5 integer number  :\n");
+    
+	for(n=0;n<5;n++)
+	
+		scanf("%d",&a[n]);
+	    printf("arrray elment are:\n");
+
+	           
+			    for(n=5;n>-0;n--)
+	    
+	    	        printf("%d\n",a[n]);
+	
+
+}
