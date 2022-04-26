349b9549770f84234e6a647c5359974162c89678
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-04-26 08:29:50 +0530

    Que 52 shift data 2 bit

---
+++ C Program/Que52.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x ,y;
+	
+	printf("Enter the numbers:");
+	scanf("%d",&x);
+	printf("integer value of number %d\n",&x);
+	
+	y=x*x*x;
+	
+	printf("left shifted number number %d",&y);
+	
+}
+	
+	
+	
