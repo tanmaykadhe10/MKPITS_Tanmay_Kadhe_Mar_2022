475270b3a912992096be26319398d8af052811a1
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-01 20:19:51 +0530

    Que72_ass

---
+++ C Program/Que72.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int a,b,c,d;
+	
+	a=(-1+4*6);
+	//23
+	printf("%d\n",a);
+	b=((35+5)%7);
+	//40/7=5
+	printf("%d\n",b);
+	c=(14+(-4*6/11));
+	//14+(24/11)=14-2=12
+	printf("%d\n",c);
+	d=(2+(15/6)*1-(7/2));
+	d=2+2-1;
+	//2+(15/6-reminderof (7%2)=2+2-1=3
+	printf("%d\n",d);
+}
