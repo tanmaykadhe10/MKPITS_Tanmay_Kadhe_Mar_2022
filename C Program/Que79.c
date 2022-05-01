35f5019fa6671d129df2e95089cfff4e9a8fe3c2
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-01 20:07:10 +0530

    Que79_ass

---
+++ C Program/Que79.c
@@ -0,0 +1,23 @@
+//check given positive integer is multiple of 3 or 7
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	printf("Enter Any Number : ");
+	scanf("%d",&n);
+
+	if(n%3==0)
+	{
+		printf("%d is a Multiple Of 3 ",n);
+
+	}
+	else if(n%7==0)
+	{
+		printf("%d is a Multiple Of 7 ",n);
+	}
+	else
+	{
+		printf("%d is not a Multiple of 3 or 7 ",n);
+	}
+} 
