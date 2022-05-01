5c3c6fa486de9b1eb7133d52fe510ceea2d48ca2
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-01 20:26:33 +0530

    Que67_ass

---
+++ C Program/Que67.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	
+	printf("Enetr the 7 number digit=");
+	scanf("%d",&n);
+
+	printf("%d  ",(n/1000000));
+	n=n-((n/1000000)*1000000);
+	
+	printf("%d  ",(n/100000));
+	n=n-((n/100000)*100000);
+	
+	printf("%d  ",(n/10000));
+	n=n-((n/10000)*10000);
+	
+	printf("%d  ",(n/1000));
+	n=n-((n/1000)*1000);
+	
+	printf("%d  ",(n/100));
+	n=n-((n/100)*100);
+	
+	printf("%d  ",(n/10));
+	n=n-((n/10)*10);
+	
+	printf("%d  ",(n%10));
+	
+}
