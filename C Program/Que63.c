f4052b70b147675156a4a4eb3eb5f28a66a56d01
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-01 21:00:28 +0530

    Que63_ass

---
+++ C Program/Que63.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+ char name1[100];
+ char name2[100];
+ int i;
+ 
+ printf("Enter any name:");
+ scanf("%s",name1);
+ 
+ for(i=0;name1[i]!='\0';i++)
+ {
+ 	name2[i]=name1[i];
+ }
+ 
+ name2[i]='\0';
+ printf("\n");
+ printf("name2= %s",name2);
+ printf("\n Number of character =%d \n",i);
+}
