397c1a9505b572e182152066d5778f730083803b
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-01 20:18:36 +0530

    Que73_ass

---
+++ C Program/Que73.c
@@ -0,0 +1,17 @@
+//multiplication of two number
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2,n3,mul;
+	printf("Enter Any 1st Number : ");
+	scanf("%d",&n1);
+	printf("Enter Any 2nd Number : ");
+	scanf("%d",&n2);
+	printf("Enter Any 3rd Number : ");
+	scanf("%d",&n3);
+
+	mul=n1*n2*n3;
+	printf("\nMultiplication Of Number %d*%d*%d = %d\n",n1,n2,n3,mul);
+
+}
