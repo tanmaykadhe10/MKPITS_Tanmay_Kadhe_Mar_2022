c0bfabe95ef44f5763cf60c31ddcbb9086798ce6
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-01 20:12:34 +0530

    Que77_ass

---
+++ C Program/Que77.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,add, res;
+	printf("Enter the number=");
+	scanf("%d",&num1);
+	printf("Enter the number=");
+	scanf("%d",&num2);
+	
+	add=num1+num2;
+	if(add==30)
+	{
+		printf("TRUE");
+	}
+	else
+	{
+		printf("additon not equal 30");
+	}
+}
