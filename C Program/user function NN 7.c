626e371f828fb624fa52e4c3e031104f59dab9b7
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-04 12:46:04 +0530

    no argument no return type _7

---
+++ C Program/user function NN 7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void fun1()
+{
+	int x,n,i,power=1;
+	printf("Enter X & Y : ");
+	scanf("%d %d",&x,&n);
+
+	for(i=1;i<=n;i++)
+	{
+		power=power*x;
+	}
+	printf("%d",power);
+}
+
+void main()
+{
+	fun1();
+} 
