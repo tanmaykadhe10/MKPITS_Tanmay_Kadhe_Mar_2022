07d11ba20d4136ed26cadec2e6e8a8a770a4da22
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-04 13:01:57 +0530

    with argument no return _2

---
+++ C Program/user function for loop YN cond.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+void fun1();
+void factorial();
+void main()
+
+{
+	fun1();
+	factorial();
+
+}
+void fun1()
+{
+	int a[2] , i;
+	printf("Enter the 2 no. =");
+	scanf("%d",a[i]);
+	for(i=0;i>=2;i--)
+	{
+	
+	printf("Enter the 2 integer numbers :\n");
+	scanf("%d\n",&a[i]);
+	
+    }
+}
+void factorial()
+{
+     int i,a[2];
+	for(i=2;i>=0;i--)
+	 {
+	   
+	   
+	   if(a[i]%4==0)
+	 {
+	   printf("\n divisible by 4 %d\n\n",a[i]);
+	 } 
+}
+}
+
