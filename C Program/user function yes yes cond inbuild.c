247959d419adea11b765375623d5aaf9745e78a9
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-04 12:14:08 +0530

    with argument and with return type

---
+++ C Program/user function yes yes cond inbuild.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+	int fun(int x,int y)
+	{
+		int r;
+		r=x+y;
+		return r;
+	}
+	void main()
+	{
+		int a=2,b=4,res;
+	    res = fun(a,b);
+	    printf("%d",res);
+	
+	}
+
