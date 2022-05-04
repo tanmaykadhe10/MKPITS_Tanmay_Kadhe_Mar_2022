9cca492e7195e77f0e6eeed3523e70002f6239d0
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-04 12:16:08 +0530

    no argument with return type

---
+++ C Program/user function return typr NY cond.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int fun()
+{
+
+	int fun(int x,int y)
+	{
+		int a=2,b=4,res;
+		res=a+b;
+		return res;
+	}
+	void main()
+	{
+		int r;
+	    r=fun();
+	    printf("%d",r);
+	
+	}
+}
