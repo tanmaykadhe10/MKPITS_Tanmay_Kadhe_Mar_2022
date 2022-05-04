db627c4b2590acd8b8d27d63aa802259fc633a84
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-04 12:43:32 +0530

    no argument no return type _5

---
+++ C Program/user function NN cond 5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void fun1()
+{
+	int n,sum=0,r;
+	printf("Enter any number : ");
+	scanf("%d",&n);
+	while(n>0)    
+	{    
+	r=n%10;    
+
+	sum=sum*10+r;
+
+	n=n/10;    
+	}
+	printf("%d",sum);
+}
+
+void main()
+{
+   fun1();
+}
