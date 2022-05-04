0a7deddc73a521c002f2e9be5f89df6ae4f7d4bc
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-04 12:20:32 +0530

    no argument no return type 2

---
+++ C Program/user function for loopl NN cond.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void factorial();
+int main()
+
+{
+	factorial();
+
+	 
+}
+void factorial()
+{
+	int a[2] , n;
+	
+	printf("Enter the 2 integer numbers :\n");
+	scanf("%d\n",&a[n]);
+	
+	for(n=2;n>=0;n--)
+	 {
+	   
+	   
+	   if(a[n]%4==0)
+	 {
+	   printf("divisible by 4 %d\n",a[n]);
+	 } 
+}
+}
+
