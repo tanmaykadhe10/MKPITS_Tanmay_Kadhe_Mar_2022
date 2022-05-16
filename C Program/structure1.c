fe0f1c3a39d598e2d868bbc803220abd6e923736
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-16 08:40:21 +0530

    struct program

---
+++ C Program/structure1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+	int id;
+	char name[30];
+}s1;
+ void main()
+{
+	struct student ;
+	printf("Enter student id :");
+	scanf("%d",&s1.id);
+	printf("Enter student name :");
+	scanf("%s",&s1.name);
+
+	printf("student id :%d",s1.id);
+	printf(" \nstudent id :%s",s1.name);
+	
+}
+
