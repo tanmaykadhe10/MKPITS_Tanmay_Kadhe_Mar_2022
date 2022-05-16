18eef6c7e32de1f09d989e7b5b7abcd37fcb3996
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-16 08:52:25 +0530

    Que 103

---
+++ C Program/Que103.c
@@ -0,0 +1,35 @@
+/*Write a C program to rotate the elements of a given array of integers (length 4) in left direction and return the new array.*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,a[4];
+
+	for(i=0;i<4;i++)
+	{
+		printf("Enter Array Element : ");
+		scanf("%d",&a[i]);	
+	}
+
+	for(i=0;i<4;i++)
+	{
+		if(i==0)
+		{
+			j=a[i];
+		}
+
+		a[i]=a[i+1];
+
+		if(i==3)
+		{
+			a[i]= j;
+		}
+	}
+	for(i=0;i<4;i++)
+	{
+		printf("%d",a[i]);
+	}
+
+
+
+} 
