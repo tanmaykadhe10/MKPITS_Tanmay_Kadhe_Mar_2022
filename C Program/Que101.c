77fe89b255135ad443ab9c83da348439b282f2f4
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-16 08:50:24 +0530

    Que 101

---
+++ C Program/Que101.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i;
+	printf("Enter Size Of Array \n");
+	scanf("%d",&n);
+	
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		printf("Enter Array element : ");
+		scanf("%d",&a[i]);
+	}
+	
+	if(a[0]==a[n-1])
+	{
+		printf("True !");
+	}
+	else
+	{
+		printf("False !");
+	}
+}
