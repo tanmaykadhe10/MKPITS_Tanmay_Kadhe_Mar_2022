e8a20a284951543a58e3cfe14863a7041d0ff3dc
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-05-17 20:53:55 +0530

    Que 301

---
+++ C Program/Que 301.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int i , j ,k;
+	
+	for (i=1;i<=3;i++)
+	{
+		for (j=1;j>=3;j--)
+		{
+			printf(" ");
+		
+		}
+		for(k=1;k<=i;k++)
+		{
+			printf("%c",(char)(i+64));
+		}
+		printf("\n");
+	}
+}
