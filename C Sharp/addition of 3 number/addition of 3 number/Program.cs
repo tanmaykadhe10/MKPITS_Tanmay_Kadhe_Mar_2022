b9e3d0c892c8804195c31701fc65c4315d8c729b
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:51:23 +0530

    Assingment- Addition of three number

---
+++ C Sharp/addition of 3 number/addition of 3 number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition_of_3_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,num3;
+            Console.WriteLine("Enter the num1 :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the num2 :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the num3 :");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            int add = num1+num2+num3;
+            Console.WriteLine("area of rectangle is :" +add);
+            Console.ReadLine();
+        }
+    }
+}
