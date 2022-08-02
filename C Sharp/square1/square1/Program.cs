dc809723d8b7b06bdd3f8ac9a395d9dfbd202296
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:39:17 +0530

    practice - square of number

---
+++ C Sharp/square1/square1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            int square = num * num;
+            Console.WriteLine("Square of entered number =" + square);
+            Console.ReadLine();
+        }
+    }
+}
