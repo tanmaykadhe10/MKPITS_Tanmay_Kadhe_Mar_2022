4fbeec379b053cc92043478b7ff78697c2a2ac40
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:48:24 +0530

    practice - Area of Reactangle

---
+++ C Sharp/area  of rectange/area  of rectange/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area__of_rectange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, breath;
+            Console.WriteLine("Enter the length :");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the breath :");
+            breath = Convert.ToInt32(Console.ReadLine());
+            int rectangle = length * breath;
+            Console.WriteLine("area of rectangle is :" + rectangle);
+            Console.ReadLine();
+
+        }
+    }
+}
