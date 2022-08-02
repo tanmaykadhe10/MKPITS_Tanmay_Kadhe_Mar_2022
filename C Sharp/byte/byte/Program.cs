681ea0a3ec449d63edfae078371f5e0b0dc9ff8d
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:52:46 +0530

    practice- Byte,Int,short

---
+++ C Sharp/byte/byte/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @byte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             byte b1;
+            Console.WriteLine("Enter the number :");
+            b1 = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("enter byte is :" + b1);
+            Console.ReadLine();
+        }
+    }
+}
