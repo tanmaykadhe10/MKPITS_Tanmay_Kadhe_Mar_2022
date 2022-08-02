3475060a94f0e25a7d3a30475fb5415ce23e32c0
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:42:31 +0530

    practice- print character

---
+++ C Sharp/print character/print character/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter the character :");
+            ch=Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("enter character is :" +ch);
+            Console.ReadLine();
+
+                
+        }
+    }
+}
