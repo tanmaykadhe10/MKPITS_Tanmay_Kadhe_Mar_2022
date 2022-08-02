852fd85645fcfc2a7dbbbf9cf6268e698c402de6
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:47:01 +0530

    practice - Add two number

---
+++ C Sharp/add 2 number/add 2 number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_2_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.WriteLine("Enter number1 :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int num2;
+            Console.WriteLine("Enter number2 :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int add = num1 + num2;
+            Console.WriteLine("addition of two number :" + add);
+            Console.ReadLine();
+        }
+    }
+}
