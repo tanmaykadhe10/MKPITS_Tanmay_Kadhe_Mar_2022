53efa2c3b1d0705d640f982db41af82a6bfbfcbb
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:37:23 +0530

    practice print number in c sharp

---
+++ C Sharp/print number/print number/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace print_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entered number is :" +num);
+           
+        }
+    }
+}
