b9559eb635ca42e91e1af0ce0e667fa5b7dad3bb
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:49:56 +0530

    assingment- Area of circle

---
+++ C Sharp/area of circle/area of circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_of_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rad;
+            Console.WriteLine("Enter the radius of circle :");
+            rad = Convert.ToInt32(Console.ReadLine());
+            float area = 3.14f * rad * rad;
+            Console.WriteLine("area of circle is :" + area);
+            float circumferance = 2*3.14f * rad;
+            Console.WriteLine("area of circle is :" + circumferance);
+            Console.ReadLine();
+        }
+    }
+}
