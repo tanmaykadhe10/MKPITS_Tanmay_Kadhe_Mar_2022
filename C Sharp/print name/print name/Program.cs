28b3549e6c9401315474dee6619511eeb96c4f19
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-02 07:44:14 +0530

    practice - print name

---
+++ C Sharp/print name/print name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter the character :");
+            name = (Console.ReadLine());
+
+            Console.WriteLine("enter character is :" + name);
+            Console.ReadLine();
+        }
+    }
+}
