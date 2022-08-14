268ab8ddf6844ddef628570378cae4dbf53ad0ef
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-14 13:15:39 +0530

    Number posetive or negative using if-else

---
+++ Assingment/A1_Q3_number posetive or negative/A1_Q3_number posetive or negative/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_Q3_number_posetive_or_negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int n;
+
+			Console.WriteLine("Enter the number");
+			n = Convert.ToInt32(Console.ReadLine());
+
+
+
+			if (0 < n)
+			{
+
+				Console.WriteLine("Enter Number is posetive number ! =" + n);
+			}
+			else
+			{
+				Console.WriteLine("Enter Number is Not nenative number ! =" + n);
+			}
+
+			Console.ReadLine();
+		}
+    }
+}
