73b41d87249149b9f0c0e36aa569bf243e3d4a03
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-14 13:09:08 +0530

    Equal or not using if else condition

---
+++ Assingment/A1_Q1_equal or not using if else/A1_Q1_equal or not using if else/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_Q1_equal_or_not_using_if_else
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.WriteLine("Enter number 1");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            num2 = Convert.ToInt16(Console.ReadLine());
+
+
+            if (num1==num2)
+            {
+                Console.WriteLine("number is eqaul !");
+            }
+            else
+            {
+                Console.WriteLine("number is not eqaul !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
