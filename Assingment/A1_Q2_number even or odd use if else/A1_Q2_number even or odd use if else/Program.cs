720de06c9ce721a5401b37a62dceea76b1f2881c
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-08-14 13:13:27 +0530

    even odd using if-else condition

---
+++ Assingment/A1_Q2_number even or odd use if else/A1_Q2_number even or odd use if else/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_Q2_number_even_or_odd_use_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int n;
+
+			Console.WriteLine("Enter the number");
+			n = Convert.ToInt32(Console.ReadLine());
+
+
+
+			if (n % 2 == 0)
+			{
+
+				Console.WriteLine("Enter Number is Even number ! -" + n);
+			}
+			else
+			{
+				Console.WriteLine("Enter Number is Not Odd number ! -" + n);
+			}
+
+			Console.ReadLine();
+		}
+    }
+}
