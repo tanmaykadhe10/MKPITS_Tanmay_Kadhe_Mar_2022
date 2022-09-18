2bd4d4f0909c2da6efc0216cc5386054975dea77
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-09-18 12:25:36 +0530

    assingment salesman 12-9-22

---
+++ SQL/ass2.sql
@@ -0,0 +1,12 @@
+create table salesman1
+( 
+ name varchar(50),
+ commission float )
+ insert into salesman1 values( 'James Hoog ',       0.15)
+ 
+  insert into salesman1 values(' Nail Knite ',       0.13)
+   insert into salesman1 values(' Pit Alex '    ,      0.11)
+    insert into salesman1 values( ' Mc Lyon ' ,       0.14)
+	 insert into salesman1 values(' Paul Adam ' ,     0.13)
+	  insert into salesman1 values( 'Lauson Hen ' ,  0.12)
+	  select *from salesman1
