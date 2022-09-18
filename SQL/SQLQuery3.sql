fee806c73a8b47dc879cb5d2213a369d9d20c1b9
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-09-18 12:40:52 +0530

    practice 6-9-22

---
+++ SQL/SQLQuery3.sql
@@ -0,0 +1,19 @@
+create database mkpits1
+
+create table studn (rno int  , sname varchar(20), addres varchar(20),mobno varchar(10) )
+
+
+insert into studn values (1,'ram','nagpur','9876543219')
+insert into studn values (1,'ramesh','nagpur','9876543219')
+insert into studn values (1,'aanu','nagpur','9876543219')
+insert into studn values (1,'ramu','nagpur','9876543219')
+insert into studn values (1,'raj','nagpur','9876543219')
+insert into studn values (1,'rakesh','nagpur','9876543219')
+insert into studn values (1,'arti','nagpur','9876543219')
+insert into studn values (1,'aruna','nagpur','9876543219')
+insert into studn values (1,'dhanya','nagpur','9876543219')
+insert into studn values (1,'rahul','nagpur','9876543219')
+
+select *from studn
+
+
