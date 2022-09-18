8c99c5d3632e4485c6586db85341f6a58e7639ef
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-09-18 12:38:03 +0530

    practice 7-9-22

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,14 @@
+create database mkpit
+
+drop database Database1;
+
+create table smp (id int )
+
+create table stud(rno int , name varchar(20))
+
+insert into stud values(1,'amit')
+
+select * from stud
+
+insert into stud values(2,'dew')
+select * from stud
