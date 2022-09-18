026aa0aab9df5b8717b86ea9d72199e2b940dad2
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-09-18 12:41:58 +0530

    practice 5-9-22

---
+++ SQL/SQLQuery4.sql
@@ -0,0 +1,14 @@
+create table costomr(id int primary key, coname varchar(20),addr varchar(20))
+
+insert into costomr values(1,'ajit','nagpur')
+insert into costomr values(2,'kartik','nagpur')
+insert into costomr values(3,'rupesh','nagpur')
+insert into costomr values(4,'amit','nagpur')
+insert into costomr values(5,'vaishu','nagpur')
+insert into costomr values(6,'pranali','nagpur')
+insert into costomr values(7,'aniket','nagpur')
+insert into costomr values(8,'shubham','nagpur')
+insert into costomr values(19,'vidya','nagpur')
+insert into costomr values(11,'sujjit','nagpur')
+
+select *from costomr
