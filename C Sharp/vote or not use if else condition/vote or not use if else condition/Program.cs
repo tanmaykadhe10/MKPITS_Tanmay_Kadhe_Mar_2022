fe5dbbe5361f656e3131af82f62f5560c351fbed
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-09-23 11:05:47 +0530

    vote or not eligible to vote c sharp

---
+++ C Sharp/vote or not use if else condition/vote or not use if else condition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vote_or_not_use_if_else_condition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter the age :");
+            age = Convert.ToByte(Console.ReadLine());
+            if (age < 18)
+            { 
+                Console.WriteLine("it is not  eligiale to  vote");
+            }
+            else
+            { 
+                Console.WriteLine("it is   eligiale to  vote");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
