2c5e9367b638d1ee697f56c413d021f31ad8073a
tanmaykadhe10 <tanmaykadhe0@gmail.com>
2022-09-25 11:25:46 +0530

    Datagridveiw in console application in c sharp

---
+++ Ado.Net/datagrid veiw 21_9_22/datagrid veiw 21_9_22/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace datagrid_veiw_21_9_22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("server=DESKTOP-970G4ID\\SQLEXPRESS;integrated security=true;database=mkpsample");
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        SqlCommandBuilder sb=null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("select*from students", conn);
+            sb = new SqlCommandBuilder(da);
+            da.Fill(ds, "stud");
+            dataGridView1.DataSource = ds.Tables["stud"];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                da.Update(ds.Tables["stud"]);
+                MessageBox.Show("record update succ");
+
+
+            }
+            catch(Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+    }
+}
---
+++ Ado.Net/datagrid veiw 21_9_22/datatype console autoincrement  1/datatype console autoincrement  1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace datatype_console_autoincrement__1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+
+           
+            DataTable dt = new DataTable("student");
+            DataColumn id = new DataColumn
+            {
+                ColumnName = "id",
+                DataType = typeof(int),
+                AutoIncrement = true,
+                AutoIncrementSeed = 1000,
+                AutoIncrementStep = 10
+
+            };
+            dt.Columns.Add(id);
+
+            DataColumn name = new DataColumn("name");
+            name.MaxLength = 50;
+            name.AllowDBNull = false;
+            dt.Columns.Add(name);
+
+
+            DataColumn email = new DataColumn("email");
+                dt.Columns.Add(email);
+
+
+            DataRow row1 = dt.NewRow();
+
+            row1["name"] = "rahul";
+            row1["email"] = "rahul@gmail.com";
+            dt.Rows.Add(row1);
+
+            dt.Rows.Add(null, "mohit", "mohit@gmail.com");
+
+            foreach(DataRow row in dt.Rows)
+            {
+                Console.WriteLine(row["id"] + ", " + row["name"] + ", " + row["email"]);
+            }
+        }
+             catch(Exception e)
+            {
+                Console.WriteLine("oops something went wrong.\n" + e);
+            }
+            Console.ReadKey();
+         }
+        }
+}
